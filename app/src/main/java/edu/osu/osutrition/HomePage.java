bfd2288fc303f3def6dad9b09e02f6d6073854d7
matig123 <matiasgrotewold11@gmail.com>
2017-02-23 21:33:30 -0500

    skeletons for methods, log for oncreate

--- app/src/main/java/edu/osu/osutrition/HomePage.java
+++ app/src/main/java/edu/osu/osutrition/HomePage.java
@@ -2,12 +2,32 @@ package edu.osu.osutrition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            Log.d("STATE",savedInstanceState.toString());
+        }
+        Log.d("CREATION","onCreate() is being execued!");
         setContentView(R.layout.activity_home_page);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
     }
 }
