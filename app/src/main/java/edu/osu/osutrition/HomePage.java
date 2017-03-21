7cd40550944485adcfa3f20b2dcfab1fa566ca60
Brandon Moore <moore.3071@osu.edu>
2017-03-21 13:49:39 -0400

    Added activities and made main page buttons work
    Added necessary activities minus settings, and the main page
    buttons work

--- app/src/main/java/edu/osu/osutrition/HomePage.java
+++ app/src/main/java/edu/osu/osutrition/HomePage.java
@@ -1,33 +1,61 @@
 package edu.osu.osutrition;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(savedInstanceState != null) {
-            Log.d("STATE",savedInstanceState.toString());
+        if (savedInstanceState != null) {
+            Log.d("STATE", savedInstanceState.toString());
         }
-        Log.d("CREATION","onCreate() is being execued!");
+        Log.d("CREATION", "onCreate() is being execued!");
         setContentView(R.layout.activity_home_page);
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
+    }
+
+    public void toMapSearch(View view) {
+        Intent intent = new Intent(this, Search_Map.class);
+        startActivity(intent);
+    }
+
+    public void toFilterSearch(View view) {
+        Intent intent = new Intent(this, Search_Filter.class);
+        startActivity(intent);
+    }
+
+    public void toLocationInfo(View view) {
+        Intent intent = new Intent(this, Location_Info.class);
+        startActivity(intent);
+    }
+
+    public void toLocationMenu(View view) {
+        Intent intent = new Intent(this, Location_Menu.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/osu/osutrition/Search_Map.java
@@ -0,0 +1,13 @@
+package edu.osu.osutrition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Search_Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search__map);
+    }
+}
