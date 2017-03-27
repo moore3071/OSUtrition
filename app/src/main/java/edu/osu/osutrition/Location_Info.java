0f7804ebb769f2236c8533d7bbd4f9457f37bda9
matig123 <matiasgrotewold11@gmail.com>
2017-03-27 17:06:52 -0400

    buttons, i dont think they do anything yet

--- app/src/main/java/edu/osu/osutrition/Location_Info.java
+++ app/src/main/java/edu/osu/osutrition/Location_Info.java
@@ -1,7 +1,9 @@
 package edu.osu.osutrition;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Location_Info extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Location_Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location__info);
+    }
+
+    public void toLocationMenu(View view) {
+        Intent intent = new Intent(this, Location_Menu.class);
+        startActivity(intent);
+    }
+
+    public void toMap(View view) {
+        Intent intent = new Intent(this, Search_Maps.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/osu/osutrition/Search_Filter.java
+++ app/src/main/java/edu/osu/osutrition/Search_Filter.java
@@ -1,7 +1,9 @@
 package edu.osu.osutrition;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Search_Filter extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Search_Filter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search__filter);
+    }
+
+    public void toLocationInfo(View view) {
+        Intent intent = new Intent(this, Location_Info.class);
+        startActivity(intent);
     }
 }
