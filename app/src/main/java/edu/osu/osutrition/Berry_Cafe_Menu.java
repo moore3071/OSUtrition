069e40056d7f8afbd6ce07d8b97ecc582db9558b
green.1421@osu.edu <green.1421@osu.edu>
2017-03-27 20:56:24 -0400

    fixed bug

--- app/src/main/java/edu/osu/osutrition/Berry_Cafe_Menu.java
+++ app/src/main/java/edu/osu/osutrition/Berry_Cafe_Menu.java
@@ -12,6 +12,17 @@ public class Berry_Cafe_Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_berry_cafe_menu);
     }
-
+    public void toNutrition0(View view){
+        Intent intent = new Intent(this, NutritionData0.class);
+        startActivity(intent);
+    }
+    public void toNutrition1(View view){
+        Intent intent = new Intent(this, NutritionData1.class);
+        startActivity(intent);
+    }
+    public void toNutrition2(View view){
+        Intent intent = new Intent(this, NutritionData2.class);
+        startActivity(intent);
+    }
     
 }
