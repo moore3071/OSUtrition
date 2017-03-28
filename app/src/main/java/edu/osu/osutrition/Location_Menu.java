444c112f352219734b446da49830d60ae369e387
green.1421@osu.edu <green.1421@osu.edu>
2017-03-27 20:12:20 -0400

    Added button for turkey swiss sandwich

--- app/src/main/java/edu/osu/osutrition/Location_Menu.java
+++ app/src/main/java/edu/osu/osutrition/Location_Menu.java
@@ -2,6 +2,8 @@ package edu.osu.osutrition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class Location_Menu extends AppCompatActivity {
 
@@ -9,6 +11,10 @@ public class Location_Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location__menu);
+    }
+    public void toNutrition0(View view){
+        Intent intent = new Intent(this, NutritionData0.class);
+        startActivity(intent);
     }
 
     
---
+++ app/src/main/java/edu/osu/osutrition/NutritionData0.java
@@ -0,0 +1,20 @@
+package edu.osu.osutrition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class NutritionData0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrition_data0);
+    }
+
+}
